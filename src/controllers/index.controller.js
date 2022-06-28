51f7ecf6173d9399669097d006006d981822d42c
Carlos Quiroz <carloseqv29596@gmail.com>
2022-06-27 23:17:39 -0400

    [FEAT] add last queries

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,10 +1,15 @@
 const { Pool } = require('pg')
-// queries
 const {
-  get_one_table
+  INSERT_WITH_FK,
+  INSERT_WITHOUT_FK,
+  UPDATE_WITH_FK,
+  UPDATE_WITHOUT_FK,
+  GET_ONE_TABLE,
+  GET_MORE_THAN_A_TABLE,
+  GET_GROUP_ONE_TABLE,
+  GET_GROUP_MORE_THAN_A_TABLE
 } = require('../db/queries.json')
 
-//
 const pool = new Pool({
   host: 'localhost',
   user: 'postgres',
@@ -13,36 +18,114 @@ const pool = new Pool({
   port: 5432
 })
 
-/**
- * CONSULTA DE UNA TABLA
- */
-const getUsers = async (req, res) => {
-  const response = await pool.query(get_one_table)
-  res.status(200).json(response.rows);
+const createUser = async (req, res) => {
+  const { id_country, gender, birthday, name, lastname, nickname, email } = req.body
+
+  if (!id_country) {
+    res.status(400).json('id_country is required')
+    return
+  }
+
+  try {
+    const response = await pool.query(
+      INSERT_WITH_FK,
+      [id_country, gender, birthday, name, lastname, nickname, email]
+    )
+    console.log(response);
+    res.json({
+      message: 'User added succesfully',
+      body: {
+        user: { id_country, gender, birthday, name, lastname, nickname, email }
+      }
+    });
+  } catch {
+    res.status(400);
+  }
 }
 
+const createItem = async (req, res) => {
+  const { name, price, offer } = req.body
+  try {
+    const response = await pool.query(
+      INSERT_WITHOUT_FK,
+      [name, price, offer]
+    )
+    res.json({
+      message: 'Item added succesfully',
+      body: {
+        item: { name, price, offer }
+      }
+    });
+  } catch {
+    res.status(400);
+  }
+}
 
+const updateDevice = async (req, res) => {
+  const id = req.params.id
+  const { name } = req.body
+  try {
+    const response = await pool.query(UPDATE_WITH_FK, [name, id])
+    res.json('Device updated succesfully');
+  } catch {
+    res.status(400);
+  }
+}
 
-//------------------------------------------------------------------//
-/**
- * EXAMPLE OF INSERT
- */
+const updateOs = async (req, res) => {
+  const id = req.params.id
+  const { name } = req.body
+  try {
+    const response = await pool.query(UPDATE_WITHOUT_FK, [name, id])
+    res.json('OS updated succesfully');
+  } catch {
+    res.status(400);
+  }
+}
 
-// const createUser = async (req, res) => {
-//   const { name, email } = req.body
+const getItems = async (req, res) => {
+  try {
+    const response = await pool.query(GET_ONE_TABLE)
+    res.status(200).json(response.rows);
+  } catch {
+    res.status(400);
+  }
+}
 
-//   const response = await pool.query(
-//     'INSERT INTO users (name, email) VALUES ($1, $2)',
-//     [name, email]
-//   )
-//   res.json({
-//     message: 'user added succesfully',
-//     body: {
-//       user: { name, email }
-//     }
-//   });
-// }
+const getOsName = async (req, res) => {
+  try {
+    const response = await pool.query(GET_MORE_THAN_A_TABLE)
+    res.status(200).json(response.rows);
+  } catch {
+    res.status(400);
+  }
+}
+
+const getRepeatedItems = async (req, res) => {
+  try {
+    const response = await pool.query(GET_GROUP_ONE_TABLE)
+    res.status(200).json(response.rows);
+  } catch {
+    res.status(400);
+  }
+}
+
+const getDevicesMostUsed = async (req, res) => {
+  try {
+    const response = await pool.query(GET_GROUP_MORE_THAN_A_TABLE)
+    res.status(200).json(response.rows);
+  } catch {
+    res.status(400);
+  }
+}
 
 module.exports = {
-  getUsers
+  createUser,
+  createItem,
+  updateDevice,
+  updateOs,
+  getItems,
+  getOsName,
+  getRepeatedItems,
+  getDevicesMostUsed
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,54 @@ const { Router } = require('express')
 const router = Router()
 
 const {
-  getUsers
+  createItem,
+  createUser,
+  updateDevice,
+  updateOs,
+  getItems,
+  getOsName,
+  getRepeatedItems,
+  getDevicesMostUsed
 } = require('../controllers/index.controller')
 
-router.get('/users', getUsers)
+/**
+ * Insercion en un objeto sin FK.
+ */
+router.post('/item', createItem)
+
+/**
+ * Insercion en un objeto con FK.
+ */
+router.post('/user', createUser)
+
+/**
+ * Actualizar un objeto con FK.
+ */
+router.put('/device/:id', updateDevice)
+
+/**
+ * Actualizar un objeto sin FK.
+ */
+router.put('/os/:id', updateOs)
+
+/**
+ * Consulta de una sola tabla.
+ */
+router.get('/items', getItems)
+
+/**
+ * Consulta de mas de una tabla.
+ */
+router.get('/osname', getOsName)
+
+/**
+ * Consulta utilizando agrupación una sola tabla.
+ */
+router.get('/repeateditems', getRepeatedItems)
+
+/**
+ * Consulta utilizando agrupacion varias tablas.
+ */
+router.get('/devicesmostused', getDevicesMostUsed)
 
 module.exports = router
